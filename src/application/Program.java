213e7956024bc53795af49ca5ff9774d68445707
MaiadeOlive <fernandamaiadeoliveira@gmail.com>
2021-02-18 21:02:56 -0300

    Fix - better treatment for promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -42,8 +43,17 @@ public class Program {
 
 				if (chessMatch.getPromoted() != null){
 					System.out.println("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
-					chessMatch.replacePromotedPiece(type);
+					String type = sc.nextLine().toUpperCase();
+					if (
+							!type.equals("B") &&
+							!type.equals("N") &&
+							!type.equals("R") &&
+							!type.equals("Q")
+					) {
+						System.out.println("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+						chessMatch.replacePromotedPiece(type);
+					}
 				}
 			}
 			catch (ChessException e){
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,9 +1,6 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -124,7 +121,7 @@ public class ChessMatch {
                 !type.equals("R") &&
                 !type.equals("Q")
         ){
-            throw new InvalidParameterException("Invalid type for promotion.");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
